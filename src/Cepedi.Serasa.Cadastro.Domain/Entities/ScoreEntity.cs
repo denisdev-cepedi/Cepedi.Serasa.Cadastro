7515e88836eb5ca530df10b22cbfc445dc0901c8
ValberF <valberfrsantos@gmail.com>
2024-04-11 12:32:03 -0300

    add Score Entity and TypeConfiguration

---
+++ src/Cepedi.Serasa.Cadastro.Domain/Entities/ScoreEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Cepedi.Serasa.Cadastro.Domain;
+
+public class ScoreEntity
+{
+    public int Id { get; set; }
+    public required string IdPessoa { get; set; }
+    public required double Score { get; set; }
+}
