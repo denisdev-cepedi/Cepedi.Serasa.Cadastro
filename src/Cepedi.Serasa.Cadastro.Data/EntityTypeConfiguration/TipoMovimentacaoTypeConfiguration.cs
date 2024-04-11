a4ac8ddf94641185521bcd2071a3679f0ae9ca55
ValberF <valberfrsantos@gmail.com>
2024-04-11 12:10:35 -0300

    add TipoMovimentacao Entity and TypeConfiguration

---
+++ src/Cepedi.Serasa.Cadastro.Data/EntityTypeConfiguration/TipoMovimentacaoTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Cepedi.Serasa.Cadastro.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cepedi.Serasa.Cadastro.Data.EntityTypeConfiguration;
+public class TipoMovimentacaoTypeConfiguration : IEntityTypeConfiguration<TipoMovimentacaoEntity>
+{
+    public void Configure(EntityTypeBuilder<TipoMovimentacaoEntity> builder)
+    {
+        builder.ToTable("Usuario");
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.NomeTipo).IsRequired().HasMaxLength(255);
+    }
+}
+
---
+++ src/Cepedi.Serasa.Cadastro.Domain/Entities/TipoMovimentacaoEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Cepedi.Serasa.Cadastro.Domain.Entities;
+public class TipoMovimentacaoEntity
+{
+    public int Id { get; set; }
+    public required string NomeTipo { get; set; }
+}
