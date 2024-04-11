43ddcfe3e239fa139bbe7d3193092f0c5515eef4
Victor Quirino <victorsouzaq@gmail.com>
2024-04-11 09:46:59 -0300

    feature - Entidade Pessoa

---
+++ src/Cepedi.Serasa.Cadastro.Domain/Entities/PessoaEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cepedi.Serasa.Cadastro.Domain.Entities;
+public class PessoaEntity
+{
+    public int Id { get; set; }
+    public required string Nome { get; set; }
+    public required string CPF { get; set; }
+}
