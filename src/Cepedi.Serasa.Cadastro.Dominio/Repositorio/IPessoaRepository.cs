f1048ec71c7b608363e79a82ae030740a1c71024
Victor Quirino <victorsouzaq@gmail.com>
2024-04-17 17:01:24 -0300

    add repositório de Pessoa

---
+++ src/Cepedi.Serasa.Cadastro.Dominio/Repositorio/IPessoaRepository.cs
@@ -0,0 +1,12 @@
+﻿using Cepedi.Serasa.Cadastro.Dominio.Entidades;
+
+namespace Cepedi.Serasa.Cadastro.Domain.Repositorio;
+
+public interface IPessoaRepository
+{
+    Task<PessoaEntity> ObterPessoaAsync(int id);
+    Task<List<PessoaEntity>> ObterPessoasAsync();
+    Task<PessoaEntity> CriarPessoaAsync(PessoaEntity pessoa);
+    Task<PessoaEntity> AtualizarPessoaAsync(PessoaEntity pessoa);
+    Task ExcluirPessoaAsync(PessoaEntity pessoa);
+}
