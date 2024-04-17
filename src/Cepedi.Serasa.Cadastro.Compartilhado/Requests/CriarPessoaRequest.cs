6f8a412d90750ea425e9010843266aea314e69d8
Victor Quirino <victorsouzaq@gmail.com>
2024-04-17 14:15:42 -0300

    add request e response de Pessoa

---
+++ src/Cepedi.Serasa.Cadastro.Compartilhado/Requests/CriarPessoaRequest.cs
@@ -0,0 +1,10 @@
+﻿using Cepedi.Serasa.Cadastro.Compartilhado.Responses;
+using MediatR;
+using OperationResult;
+
+namespace Cepedi.Serasa.Cadastro.Compartilhado.Requests;
+public class CriarPessoaRequest : IRequest<Result<CriarPessoaResponse>>
+{
+    public required string Nome { get; set; }
+    public required string CPF { get; set; }
+}
