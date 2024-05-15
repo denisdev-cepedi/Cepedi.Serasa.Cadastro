f9646f443a3eb4014b11c83b017a532e1d023827
kayquepiton <kayquepiton@gmail.com>
2024-05-15 16:38:11 -0300

    Fixing Test ObterTodasMovimentacoes

--- src/Cepedi.Serasa.Cadastro.Dominio/Handlers/Movimentacao/ObterTodasMovimentacoesRequestHandler.cs
+++ src/Cepedi.Serasa.Cadastro.Dominio/Handlers/Movimentacao/ObterTodasMovimentacoesRequestHandler.cs
@@ -4,47 +4,40 @@ using Cepedi.Serasa.Cadastro.Dominio.Repositorio;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using OperationResult;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace Cepedi.Serasa.Cadastro.Dominio.Handlers.Movimentacao
+namespace Cepedi.Serasa.Cadastro.Dominio.Handlers.Movimentacao;
+public class ObterTodasMovimentacoesRequestHandler : IRequestHandler<ObterTodasMovimentacoesRequest, Result<List<ObterTodasMovimentacoesResponse>>>
 {
-    public class ObterTodasMovimentacoesRequestHandler : IRequestHandler<ObterTodasMovimentacoesRequest, Result<List<ObterTodasMovimentacoesResponse>>>
+    private readonly ILogger<ObterTodasMovimentacoesRequestHandler> _logger;
+    private readonly IMovimentacaoRepository _MovimentacaoRepository;
+
+    public ObterTodasMovimentacoesRequestHandler(ILogger<ObterTodasMovimentacoesRequestHandler> logger, IMovimentacaoRepository MovimentacaoRepository)
     {
-        private readonly ILogger<ObterTodasMovimentacoesRequestHandler> _logger;
-        private readonly IMovimentacaoRepository _movimentacaoRepository;
+        _logger = logger;
+        _MovimentacaoRepository = MovimentacaoRepository;
+    }
 
-        public ObterTodasMovimentacoesRequestHandler(ILogger<ObterTodasMovimentacoesRequestHandler> logger, IMovimentacaoRepository movimentacaoRepository)
+    public async Task<Result<List<ObterTodasMovimentacoesResponse>>> Handle(ObterTodasMovimentacoesRequest request, CancellationToken cancellationToken)
+    {
+        var movimentacoes = await _MovimentacaoRepository.ObterTodasMovimentacoesAsync();
+
+        if (movimentacoes == null)
         {
-            _logger = logger;
-            _movimentacaoRepository = movimentacaoRepository;
+            return Result.Error<List<ObterTodasMovimentacoesResponse>>(new Compartilhado.Exececoes.SemResultadoExcecao());
         }
 
-        public async Task<Result<List<ObterTodasMovimentacoesResponse>>> Handle(ObterTodasMovimentacoesRequest request, CancellationToken cancellationToken)
+        var response = new List<ObterTodasMovimentacoesResponse>();
+        foreach (var movimentacao in movimentacoes)
         {
-            var movimentacoes = await _movimentacaoRepository.ObterTodasMovimentacoesAsync();
-
-            if (movimentacoes == null)
-            {
-                // Retorna uma lista vazia se não houver movimentações
-                _logger.LogInformation("Nenhuma movimentação encontrada.");
-                return Result.Success(new List<ObterTodasMovimentacoesResponse>());
-            }
-
-            var response = new List<ObterTodasMovimentacoesResponse>();
-            foreach (var movimentacao in movimentacoes)
-            {
-                response.Add(new ObterTodasMovimentacoesResponse(
-                    movimentacao.Id, 
-                    movimentacao.IdTipoMovimentacao, 
-                    movimentacao.IdPessoa, 
-                    movimentacao.DataHora, 
-                    movimentacao.NomeEstabelecimento, 
-                    movimentacao.Valor));
-            }
-
-            return Result.Success(response);
+            response.Add(new ObterTodasMovimentacoesResponse(
+                movimentacao.Id, 
+                movimentacao.IdTipoMovimentacao, 
+                movimentacao.IdPessoa, 
+                movimentacao.DataHora, 
+                movimentacao.NomeEstabelecimento, 
+                movimentacao.Valor));
         }
+
+        return Result.Success(response);
     }
 }
