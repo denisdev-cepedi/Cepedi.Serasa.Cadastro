bf4ba94e3c97d7242ea946e476164fa7e434dcec
kayquepiton <kayquepiton@gmail.com>
2024-05-16 21:55:05 -0300

    Validation Handlers

--- src/Cepedi.Serasa.Cadastro.Dominio/Handlers/Movimentacao/ObterMovimentacaoRequestHandler.cs
+++ src/Cepedi.Serasa.Cadastro.Dominio/Handlers/Movimentacao/ObterMovimentacaoRequestHandler.cs
@@ -1,4 +1,5 @@
-﻿using Cepedi.Serasa.Cadastro.Compartilhado.Exececoes;
+﻿using Cepedi.Serasa.Cadastro.Compartilhado.Enums;
+using Cepedi.Serasa.Cadastro.Compartilhado.Exececoes;
 using Cepedi.Serasa.Cadastro.Compartilhado.Requests.Movimentacao;
 using Cepedi.Serasa.Cadastro.Compartilhado.Responses.Movimentacao;
 using Cepedi.Serasa.Cadastro.Dominio.Repositorio;
@@ -9,39 +10,41 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Cepedi.Serasa.Cadastro.Dominio.Handlers.Movimentacao
+namespace Cepedi.Serasa.Cadastro.Dominio.Handlers.Movimentacao;
+public class ObterMovimentacaoRequestHandler : IRequestHandler<ObterMovimentacaoRequest, Result<ObterMovimentacaoResponse>>
 {
-    public class ObterMovimentacaoRequestHandler : IRequestHandler<ObterMovimentacaoRequest, Result<ObterMovimentacaoResponse>>
+    private readonly ILogger<ObterMovimentacaoRequestHandler> _logger;
+    private readonly IMovimentacaoRepository _movimentacaoRepository;
+
+    public ObterMovimentacaoRequestHandler(ILogger<ObterMovimentacaoRequestHandler> logger, IMovimentacaoRepository movimentacaoRepository)
     {
-        private readonly ILogger<ObterMovimentacaoRequestHandler> _logger;
-        private readonly IMovimentacaoRepository _movimentacaoRepository;
+        _logger = logger;
+        _movimentacaoRepository = movimentacaoRepository;
+    }
 
-        public ObterMovimentacaoRequestHandler(ILogger<ObterMovimentacaoRequestHandler> logger, IMovimentacaoRepository movimentacaoRepository)
+    public async Task<Result<ObterMovimentacaoResponse>> Handle(ObterMovimentacaoRequest request, CancellationToken cancellationToken)
+    {
+        // Verificar se a movimentação existe com base no ID fornecido na solicitação
+        var movimentacaoEntity = await _movimentacaoRepository.ObterMovimentacaoAsync(request.Id);
+
+        // Se a movimentação não existe, retornar um erro indicando a falta de resultados
+        if (movimentacaoEntity == null)
         {
-            _logger = logger;
-            _movimentacaoRepository = movimentacaoRepository;
+            return Result.Error<ObterMovimentacaoResponse>(
+                new Compartilhado.Exececoes.ExcecaoAplicacao(CadastroErros.IdMovimentacaoInvalido));
         }
 
-        public async Task<Result<ObterMovimentacaoResponse>> Handle(ObterMovimentacaoRequest request, CancellationToken cancellationToken)
-        {
-            var movimentacaoEntity = await _movimentacaoRepository.ObterMovimentacaoAsync(request.Id);
+        // Criar uma resposta com os dados da movimentação obtida
+        var response = new ObterMovimentacaoResponse(
+            movimentacaoEntity.Id,
+            movimentacaoEntity.IdTipoMovimentacao,
+            movimentacaoEntity.IdPessoa,
+            movimentacaoEntity.DataHora,
+            movimentacaoEntity.NomeEstabelecimento,
+            movimentacaoEntity.Valor
+        );
 
-            if (movimentacaoEntity == null)
-            {
-                _logger.LogInformation($"Movimentacao com Id {request.Id} não encontrada.");
-                return Result.Error<ObterMovimentacaoResponse>(new SemResultadoExcecao());
-            }
-
-            var response = new ObterMovimentacaoResponse(
-                movimentacaoEntity.Id,
-                movimentacaoEntity.IdTipoMovimentacao,
-                movimentacaoEntity.IdPessoa,
-                movimentacaoEntity.DataHora,
-                movimentacaoEntity.NomeEstabelecimento,
-                movimentacaoEntity.Valor
-            );
-
-            return Result.Success(response);
-        }
+        // Retornar um resultado de sucesso contendo a resposta da movimentação obtida
+        return Result.Success(response);
     }
 }
