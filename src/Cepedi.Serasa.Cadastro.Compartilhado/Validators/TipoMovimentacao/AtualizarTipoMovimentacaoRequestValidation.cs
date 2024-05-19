9116dfa0c69657ecf6a2415a3ff5e107ba92e2b2
ValberF <valberfrsantos@gmail.com>
2024-05-19 09:20:01 -0300

    feature/SERegister-13

---
+++ src/Cepedi.Serasa.Cadastro.Compartilhado/Validators/TipoMovimentacao/AtualizarTipoMovimentacaoRequestValidation.cs
@@ -0,0 +1,22 @@
+﻿using Cepedi.Serasa.Cadastro.Compartilhado.Requests.TipoMovimentacao;
+using FluentValidation;
+
+namespace Cepedi.Serasa.Cadastro.Compartilhado.Validators.TipoMovimentacao;
+
+public class AtualizarTipoMovimentacaoRequestValidation : AbstractValidator<AtualizarTipoMovimentacaoRequest>
+{
+    public AtualizarTipoMovimentacaoRequestValidation()
+    {
+        RuleFor(tipoMovimentacao => tipoMovimentacao.Id)
+                .NotNull().WithMessage("O ID deve ser informado")
+                .GreaterThan(0).WithMessage("ID do tipo de movimentação inválido");
+
+        RuleFor(tipoMovimentacao => tipoMovimentacao.NomeTipo)
+            .NotEmpty()
+            .WithMessage("O nome do tipo de movimentação é obrigatório.")
+            .MinimumLength(5)
+            .WithMessage("O nome do tipo de movimentação deve ter pelo menos 5 caracteres.")
+            .MaximumLength(30)
+            .WithMessage("O nome do tipo de movimentação deve ter até 5 caracteres.");
+    }
+}
---
+++ src/Cepedi.Serasa.Cadastro.Compartilhado/Validators/TipoMovimentacao/CriarTipoMovimentacaoRequestValidation.cs
@@ -0,0 +1,18 @@
+﻿using Cepedi.Serasa.Cadastro.Compartilhado.Requests.TipoMovimentacao;
+using FluentValidation;
+
+namespace Cepedi.Serasa.Cadastro.Compartilhado.Validators.TipoMovimentacao;
+
+public class CriarTipoMovimentacaoRequestValidation : AbstractValidator<CriarTipoMovimentacaoRequest>
+{
+    public CriarTipoMovimentacaoRequestValidation()
+    {
+        RuleFor(tipoMovimentacao => tipoMovimentacao.NomeTipo)
+            .NotEmpty()
+            .WithMessage("O nome do tipo de movimentação é obrigatório.")
+            .MinimumLength(5)
+            .WithMessage("O nome do tipo de movimentação deve ter pelo menos 5 caracteres.")
+            .MaximumLength(30)
+            .WithMessage("O nome do tipo de movimentação deve ter até 5 caracteres.");
+    }
+}
